0dc923630aaafb2ef60a827ecd91ffa804880458
kimuli joshua <kimulijoshua.00@gmail.com>
2025-04-27 16:59:22 +0300

    initial commit

--- sequelize.js
+++ sequelize.js
@@ -1,11 +1,9 @@
 const { Sequelize } = require('sequelize');
-const path = require('path');
 
-// SQLite Database connection
+// Reading the DATABASE_URL from the environment variables
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: path.join(__dirname, 'database.sqlite'), // Define the SQLite file location
-  logging: false, // Optional: Disable SQL query logging
+  storage: process.env.DATABASE_URL || './db/hr_system.db',  // Default to './db/hr_system.db' if not set
 });
 
 module.exports = sequelize;
